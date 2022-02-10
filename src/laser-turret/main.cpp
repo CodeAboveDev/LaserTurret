79fb2821cda91f54e277686be4d27f00ec23ad22
luuke <460035+luuke@users.noreply.github.com>
2022-02-10 18:42:50 +0100

    Add laser brightness control using PWM

--- src/laser-turret/main.cpp
+++ src/laser-turret/main.cpp
@@ -6,13 +6,27 @@ int main()
     Board::Leds::setOutput();
 
     using Laser = GpioOutputA5;
-    Laser::setOutput();
+    // Laser::setOutput();
 
+    Timer2::connect<Laser::Ch1>();
+    Timer2::enable();
+    Timer2::setMode(Timer2::Mode::UpCounter);
+
+    Timer2::setPrescaler(8);
+	Timer2::setOverflow(65535);
+    Timer2::configureOutputChannel(1, Timer2::OutputCompareMode::Pwm, 0);
+
+    Timer2::applyAndReset();
+    Timer2::start();
+
+    uint16_t brightness = 0u;
     while (true)
     {
         Board::Leds::toggle();
-        Laser::toggle();
+        // Laser::toggle();
         modm::delay_ms(Board::Button::read() ? 250 : 500);
+        brightness += 5000u;
+        Timer2::setCompareValue(1, brightness);
     }
 
     return 0;
