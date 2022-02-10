9d614d9b891fd8ccaad97f1efb2691560219a800
luuke <460035+luuke@users.noreply.github.com>
2022-02-10 19:23:21 +0100

    Add servos control using PWM

--- src/laser-turret/main.cpp
+++ src/laser-turret/main.cpp
@@ -5,6 +5,7 @@ int main()
     Board::initialize();
     Board::Leds::setOutput();
 
+    /***** LASER DIODE *****/
     using Laser = GpioOutputA5;
     // Laser::setOutput();
 
@@ -20,13 +21,54 @@ int main()
     Timer2::start();
 
     uint16_t brightness = 0u;
+
+    /***** SERVOS *****/
+    using BottomServo = GpioOutputA8;
+    using TopServo = GpioOutputA9;
+
+    Timer1::connect<BottomServo::Ch1, TopServo::Ch2>();
+    Timer1::enable();
+    Timer1::enableOutput();
+    Timer1::setMode(Timer1::Mode::UpCounter);
+
+    Timer1::setPeriod<Board::SystemClock>(20000);
+    Timer1::configureOutputChannel(1, Timer1::OutputCompareMode::Pwm, 0);
+    Timer1::configureOutputChannel(2, Timer1::OutputCompareMode::Pwm, 0);
+
+    Timer1::applyAndReset();
+    Timer1::start();
+
+    Timer1::setCompareValue(1, 4846);
+    Timer1::setCompareValue(2, 4846);
+    uint8_t i = 0u;
+
     while (true)
     {
         Board::Leds::toggle();
         // Laser::toggle();
-        modm::delay_ms(Board::Button::read() ? 250 : 500);
+        modm::delay_ms(1000);
         brightness += 5000u;
         Timer2::setCompareValue(1, brightness);
+
+        switch(i)
+        {
+            case 0:
+                Timer1::setCompareValue(1, 4846);
+                Timer1::setCompareValue(2, 4846);
+                break;
+            case 1:
+                Timer1::setCompareValue(1, 3230);
+                Timer1::setCompareValue(2, 3230);
+                break;
+            case 2:
+                Timer1::setCompareValue(1, 6461);
+                Timer1::setCompareValue(2, 6461);
+                break;
+            default:
+                break;
+        }
+        i++;
+        if(i>2) i=0u;
     }
 
     return 0;
