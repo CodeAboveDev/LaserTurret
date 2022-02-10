21168cc8f94fa13aa23b8e3f061097bb2a36d365
luuke <460035+luuke@users.noreply.github.com>
2022-02-10 17:38:49 +0100

    Remove unnecessary code

--- src/laser-turret/main.cpp
+++ src/laser-turret/main.cpp
@@ -8,12 +8,8 @@ int main()
     while (true)
     {
         Board::Leds::toggle();
-        modm::delayMilliseconds(Board::Button::read() ? 250 : 500);
-#ifdef MODM_BOARD_HAS_LOGGER
-        static uint32_t counter(0);
-        MODM_LOG_INFO << "Loop counter: " << (counter++) << modm::endl;
-#endif
+        modm::delay_ms(Board::Button::read() ? 250 : 500);
     }
+    
     return 0;
 }
-
