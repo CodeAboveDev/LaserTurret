97f883d11ba41cf7e35ac4ea54ec728f0f8bbe5c
luuke <460035+luuke@users.noreply.github.com>
2022-02-10 18:17:09 +0100

    Add laser diode toggling

--- src/laser-turret/main.cpp
+++ src/laser-turret/main.cpp
@@ -5,11 +5,15 @@ int main()
     Board::initialize();
     Board::Leds::setOutput();
 
+    using Laser = GpioOutputA5;
+    Laser::setOutput();
+
     while (true)
     {
         Board::Leds::toggle();
+        Laser::toggle();
         modm::delay_ms(Board::Button::read() ? 250 : 500);
     }
-    
+
     return 0;
 }
